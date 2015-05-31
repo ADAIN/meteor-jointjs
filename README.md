75cc6517e2a0c06d04286432a0086b310601ce68
SungYongJang <jsy@adain.kr>
2015-05-30 18:26:12 -0700

    INIT

---
+++ README.md
@@ -0,0 +1,5 @@
+# jointJS for meteor
+http://jointjs.com/
+
+## current version
+v0.9.3 with plugins
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'adain:meteor-jointjs',
+  version: '0.9.3',
+  summary: 'jointJS for meteor',
+  git: 'https://github.com/ADAIN/meteor-jointjs.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use('backbone@0.9.2', 'client');
+  api.use('stevezhu:lodash', 'client');
+  api.addFiles(['jointjs.js', 'jointjs.css', 'geometry.js'], 'client');
+
+  api.export('g', 'client');
+});
+
+Package.onTest(function(api) {
+});
